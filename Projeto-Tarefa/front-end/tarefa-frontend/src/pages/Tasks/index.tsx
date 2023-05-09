e0e53a7f3161adc925eb34aed7404872a9ec372b
FFernandoSilva <fdasilva@imaginationmedia.com>
2023-05-09 03:14:21 -0300

    Finish CRUD

--- Projeto-Tarefa/front-end/tarefa-frontend/src/pages/Tasks/index.tsx
+++ Projeto-Tarefa/front-end/tarefa-frontend/src/pages/Tasks/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import api from '../../services/api';
 import moment from 'moment';
+import './index.css';
+ 
 interface ITask{
     id: number;
     title: string;
@@ -10,24 +13,56 @@ interface ITask{
     created_at: Date;
     updated_at: Date;
 }
+ 
 const Tasks: React.FC = () => {
+ 
     const [tasks, setTasks] = useState<ITask[]>([])
+    const history = useHistory()
+ 
     useEffect(() => {
         loadTasks()
     }, [])
+ 
     async function loadTasks() {
         const response = await api.get('/tasks')
         console.log(response);
         setTasks(response.data)
     }
+ 
     function formatDate(date: Date){
         return moment(date).format('DD/MM/YYYY')
     }
+ 
+    function newTask(){
+        history.push('/tarefas_cadastro')
+    }
+ 
+    function editTask(id: number){
+        history.push(`/tarefas_cadastro/${id}`)
+    }
+
+    function viewTask(id: number){
+        history.push(`/tarefas/${id}`)
+    }
+
+    async function finishedTask(id: number){
+        await api.patch(`/tasks/${id}`)
+        loadTasks()
+    }
+
+    async function deleteTask(id: number){
+        await api.delete(`/tasks/${id}`)
+        loadTasks()
+    }
+
     return (
         
         <div className="container">
             <br />
-            <h1>Página de Tarefas</h1>
+            <div className="task-header">
+                <h1>Tarefas</h1>
+                <Button variant="dark" size="sm" onClick={newTask}>Nova Tarefa</Button>
+            </div>
             <br />
             <Table striped bordered hover className="text-center">
                 <thead>
@@ -48,10 +83,10 @@ const Tasks: React.FC = () => {
                                 <td>{formatDate(task.updated_at)}</td>
                                 <td>{task.finished ? "Finalizado" : "Pendente"}</td>
                                 <td>
-                                    <Button size="sm" variant="primary">Editar</Button>{' '}
-                                    <Button size="sm" variant="success">Finalizar</Button>{' '}
-                                    <Button size="sm" variant="warning">Visualizar</Button>{' '}
-                                    <Button size="sm" variant="danger">Remover</Button>{' '}
+                                    <Button size="sm" disabled={task.finished} variant="primary" onClick={() => editTask(task.id)}>Editar</Button>{' '}
+                                    <Button size="sm" disabled={task.finished} variant="success" onClick={() => finishedTask(task.id)}>Finalizar</Button>{' '}
+                                    <Button size="sm" variant="warning" onClick={() => viewTask(task.id)}>Visualizar</Button>{' '}
+                                    <Button size="sm" variant="danger" onClick={() => deleteTask(task.id)}>Remover</Button>{' '}
                                 </td>
                             </tr>
                         ))
@@ -61,4 +96,5 @@ const Tasks: React.FC = () => {
         </div>
     );
 }
+ 
 export default Tasks;
